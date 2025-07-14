d4817652dab9712445af454755d206a07ea2fd1e
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-14 12:39:24 +0200

    initial commit

---
+++ src/main/java/org/pati/polish_to_utf_encoding/PolishToUtfEncodingApplication.java
@@ -0,0 +1,13 @@
+package org.pati.polish_to_utf_encoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolishToUtfEncodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolishToUtfEncodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pati/polish_to_utf_encoding/PolishToUtfEncodingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pati.polish_to_utf_encoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolishToUtfEncodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
