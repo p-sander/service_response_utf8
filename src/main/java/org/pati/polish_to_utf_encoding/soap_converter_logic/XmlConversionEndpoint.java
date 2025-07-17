bc2968a443d3a85675f17f912e11ad46100ab94f
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 21:03:18 +0200

    Create XmlConversionEndpoint with @Endpoint annotation

---
+++ src/main/java/org/pati/polish_to_utf_encoding/soap_converter_logic/XmlConversionEndpoint.java
@@ -0,0 +1,7 @@
+package org.pati.polish_to_utf_encoding.soap_converter_logic;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+
+@Endpoint
+public class XmlConversionEndpoint {
+}
