488c4206dbc7d7323b02a22e131866968349c874
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 22:09:04 +0200

    Create RestExceptionHandler class

---
+++ src/main/java/org/pati/polish_to_utf_encoding/json_converter_logic/RestExceptionHandler.java
@@ -0,0 +1,9 @@
+package org.pati.polish_to_utf_encoding.json_converter_logic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+@ControllerAdvice
+@Slf4j
+public class RestExceptionHandler {
+
+}
