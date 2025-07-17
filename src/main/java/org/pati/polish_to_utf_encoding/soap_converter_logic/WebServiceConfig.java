183fd38c03cc7b812c0b362c9b49fb71b70409c8
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 20:56:18 +0200

    Enable Spring Web Services with basic configuration class

---
+++ src/main/java/org/pati/polish_to_utf_encoding/soap_converter_logic/WebServiceConfig.java
@@ -0,0 +1,9 @@
+package org.pati.polish_to_utf_encoding.soap_converter_logic;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
+
+@Configuration
+@EnableWs
+public class WebServiceConfig {
+}
