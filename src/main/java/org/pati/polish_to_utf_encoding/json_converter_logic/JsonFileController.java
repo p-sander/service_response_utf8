0cb9ecd3feb8cfacb2148c9a8197ff468f29af81
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 20:52:41 +0200

    Add post receiveJson method to JsonFileController

--- src/main/java/org/pati/polish_to_utf_encoding/json_converter_logic/JsonFileController.java
+++ src/main/java/org/pati/polish_to_utf_encoding/json_converter_logic/JsonFileController.java
@@ -11,6 +11,15 @@ import java.nio.charset.Charset;
 @RequestMapping("/json")
 public class JsonFileController {
 
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @ResponseBody
+    public ResponseEntity<String> receiveJson(@RequestBody byte[] jsonFile) {
+
+        String jsonFileInIso = new String(jsonFile, Charset.forName("ISO-8859-2"));
+
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(jsonFileInIso);
+    }
+
     @GetMapping(value = "/hi")
     public void sayHi() {
         System.out.println("hi, the rest service is working...");
