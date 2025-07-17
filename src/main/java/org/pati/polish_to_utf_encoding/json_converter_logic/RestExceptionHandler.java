9b740eceeee8afcb766fa0e8543b4bc129ab04f1
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 22:11:00 +0200

    Add global exception handling for unreadable JSON requests

--- src/main/java/org/pati/polish_to_utf_encoding/json_converter_logic/RestExceptionHandler.java
+++ src/main/java/org/pati/polish_to_utf_encoding/json_converter_logic/RestExceptionHandler.java
@@ -1,9 +1,31 @@
 package org.pati.polish_to_utf_encoding.json_converter_logic;
 
 import lombok.extern.slf4j.Slf4j;
+import org.pati.polish_to_utf_encoding.json_converter_logic.model.ApiError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.nio.charset.MalformedInputException;
+
 @ControllerAdvice
 @Slf4j
 public class RestExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiError> handleMessageNotReadable(HttpMessageNotReadableException ex) {
+        log.error("Failed to read request body: {}", ex.getMessage());
+
+        String errorMessage = "Invalid request body. Ensure it is a valid JSON.";
+
+        if (ex.getCause() instanceof MalformedInputException) {
+            errorMessage = "Character encoding error. The payload must be in ISO-8859-2.";
+        }
+
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, errorMessage);
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
 
 }
