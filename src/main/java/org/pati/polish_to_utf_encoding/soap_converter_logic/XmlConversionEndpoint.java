86085f2cd94207e71b3688e637846b2f992f6819
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 21:04:08 +0200

    Define SOAP handler method for ConvertXmlRequest

--- src/main/java/org/pati/polish_to_utf_encoding/soap_converter_logic/XmlConversionEndpoint.java
+++ src/main/java/org/pati/polish_to_utf_encoding/soap_converter_logic/XmlConversionEndpoint.java
@@ -1,7 +1,20 @@
 package org.pati.polish_to_utf_encoding.soap_converter_logic;
 
+import org.pati.polish_to_utf_encoding.soap_converter_logic.conversion.ConvertXmlRequest;
+import org.pati.polish_to_utf_encoding.soap_converter_logic.conversion.ConvertXmlResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class XmlConversionEndpoint {
+
+    private static final String NAMESPACE_URI = "http://www.example.com/soap/conversion";
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "ConvertXmlRequest")
+    @ResponsePayload
+    public ConvertXmlResponse convertXmlEncoding(@RequestPayload ConvertXmlRequest request) {
+        return new ConvertXmlResponse();
+    }
 }
