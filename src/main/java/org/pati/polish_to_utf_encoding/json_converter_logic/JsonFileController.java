f59a59d649f527628f962e048463ab07833bdef1
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 20:52:09 +0200

    Add get method to JsonFileController

--- src/main/java/org/pati/polish_to_utf_encoding/json_converter_logic/JsonFileController.java
+++ src/main/java/org/pati/polish_to_utf_encoding/json_converter_logic/JsonFileController.java
@@ -1,11 +1,20 @@
 package org.pati.polish_to_utf_encoding.json_converter_logic;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.Charset;
 
 
 @RestController
 @RequestMapping("/json")
 public class JsonFileController {
+
+    @GetMapping(value = "/hi")
+    public void sayHi() {
+        System.out.println("hi, the rest service is working...");
+    }
 
 }
 
