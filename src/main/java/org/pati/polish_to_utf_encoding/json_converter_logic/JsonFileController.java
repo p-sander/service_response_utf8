949fabfa76af17440ae23d8574e154cd85554b7c
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 20:51:29 +0200

    Create JsonFileController class

---
+++ src/main/java/org/pati/polish_to_utf_encoding/json_converter_logic/JsonFileController.java
@@ -0,0 +1,11 @@
+package org.pati.polish_to_utf_encoding.json_converter_logic;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/json")
+public class JsonFileController {
+
+}
+
